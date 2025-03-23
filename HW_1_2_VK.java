3b90c5ff74923889f7e6f58562157917f8fcae2d
consumerpicky <consumerpicky@pm.me>
2025-03-23 21:16:35 +0200

    Add files via upload

---
+++ HW_1_2_VK.java
@@ -0,0 +1,10 @@
+public class HW_1_2_VK {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+        int availableCash;
+        availableCash = (income - spending);
+        System.out.println("Итого (руб):");
+        System.out.println(availableCash);
+    }
+}
